d6eb8f0e4c932f202d01d40be47e7386bf253306
yinparafly <yinparafly@users.noreply.github.com>
2026-07-28 21:51:36 +0800

    Task 1: Project scaffolding for STM32F103 ABZ encoder data recorder
    - Create project directory structure (Core, Drivers, FatFS)
    - Copy CMSIS core_cm3 core support files
    - Copy STM32F10x device headers (stm32f10x.h, system_stm32f10x.h)
    - Copy startup_stm32f10x_md.s (GCC/RIDE7)
    - Copy Standard Peripheral Library (all inc/ and src/)
    - Copy system_stm32f10x.c from reference project USER dir
    - Create stm32f10x_conf.h with enabled peripheral headers
    - Create linker.ld for STM32F103C8T6 (64KB Flash, 20KB RAM)
    - Create Makefile with toolchain and build configuration
    - Create source stubs (main.c, encoder.c, ring_buffer.c, etc.)
    - Build verified: firmware.hex produced successfully (732 bytes text)
    - Fix: core_cm3.c strex inline asm constraint (add earlyclobber &)
      to prevent illegal register aliasing under -Os

---
+++ project/Core/Inc/stm32f10x_conf.h
@@ -0,0 +1,17 @@
+#ifndef __STM32F10x_CONF_H
+#define __STM32F10x_CONF_H
+
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_tim.h"
+#include "stm32f10x_exti.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_spi.h"
+#include "stm32f10x_dma.h"
+#include "stm32f10x_flash.h"
+#include "stm32f10x_iwdg.h"
+#include "misc.h"
+
+#define assert_param(expr) ((void)0)
+
+#endif
